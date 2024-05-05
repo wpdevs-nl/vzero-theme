dbd7f007e735eea5b9b146137cf4e238b117e05b
Koen Schipper <koenschipper@mbp-van-koen-1.kpn>
2024-05-05 15:46:07 +0200

    minor page changes

--- archive.php
+++ archive.php
@@ -1,1 +1,21 @@
 <?php
+/**
+ * Basic archive template file
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/
+ *
+ * @package vzero
+ */
+get_header();
+
+while ( have_posts() ) :
+    the_post();
+
+    if ( ! have_rows( 'content_blocks' ) ):
+        get_template_part( 'template-parts/content', get_post_type() );
+    else:
+        flexible_content();
+    endif;
+endwhile;
+
+get_footer();
